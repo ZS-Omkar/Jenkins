ab4ee5a585f4240d9ec51682527bd741cddddfc8
Omkar <omkar.betha@gmail.com>
2021-05-29 19:14:41 +0530

    ss

--- seed.groovy
+++ seed.groovy
@@ -24,3 +24,65 @@ pipelineJob('CI-Pipelines/frontend') {
     }
 }
 
+pipelineJob('CI-Pipelines/users') {
+    configure { flowdefinition ->
+        flowdefinition << delegate. 'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+            'scm'(class:'hudson.plugin.git.GitSCM',plugin:'git') {
+                'userRemoteConfigs' {
+                    'hudson.plugin.git.UserRemoteConfig' {
+                        'url'('https://github.com/ZS-Omkar/users.git')
+                    }
+                }
+                'branches' {
+                    'hudson.plugin.git.BranchSpec' {
+                        'name'('*/main')
+                    }
+                }
+            }
+            'scriptPath'('Jenkinsfile')
+            'lightweight'(true)
+        }
+    }
+}
+
+pipelineJob('CI-Pipelines/login') {
+    configure { flowdefinition ->
+        flowdefinition << delegate. 'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+            'scm'(class:'hudson.plugin.git.GitSCM',plugin:'git') {
+                'userRemoteConfigs' {
+                    'hudson.plugin.git.UserRemoteConfig' {
+                        'url'('https://github.com/ZS-Omkar/login.git')
+                    }
+                }
+                'branches' {
+                    'hudson.plugin.git.BranchSpec' {
+                        'name'('*/main')
+                    }
+                }
+            }
+            'scriptPath'('Jenkinsfile')
+            'lightweight'(true)
+        }
+    }
+}
+
+pipelineJob('CI-Pipelines/todo') {
+    configure { flowdefinition ->
+        flowdefinition << delegate. 'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+            'scm'(class:'hudson.plugin.git.GitSCM',plugin:'git') {
+                'userRemoteConfigs' {
+                    'hudson.plugin.git.UserRemoteConfig' {
+                        'url'('https://github.com/ZS-Omkar/todo.git')
+                    }
+                }
+                'branches' {
+                    'hudson.plugin.git.BranchSpec' {
+                        'name'('*/main')
+                    }
+                }
+            }
+            'scriptPath'('Jenkinsfile')
+            'lightweight'(true)
+        }
+    }
+}
