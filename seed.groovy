ea82ce911f8fa99ae699dd0f321a7b1b9fa6b662
Omkar <omkar.betha@gmail.com>
2021-05-31 16:22:36 +0530

    ss

--- seed.groovy
+++ seed.groovy
@@ -6,77 +6,14 @@ folder('CI-Pipelines') {
 pipelineJob("CI-Pipelines/frontend") {
     configure { flowdefinition ->
         flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
-            'scm'(class: 'hudson.plugin.git.GitSCM',plugin:'git') {
+            'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
                 'userRemoteConfigs' {
-                    'hudson.plugin.git.UserRemoteConfig' {
+                    'hudson.plugins.git.UserRemoteConfig' {
                         'url'('https://github.com/ZS-Omkar/frontend.git')
                     }
                 }
                 'branches' {
-                    'hudson.plugin.git.BranchSpec' {
-                        'name'('*/main')
-                    }
-                }
-            }
-            'scriptPath'('Jenkinsfile')
-            'lightweight'(true)
-        }
-    }
-}
-
-pipelineJob("CI-Pipelines/users") {
-    configure { flowdefinition ->
-        flowdefinition << delegate. 'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
-            'scm'(class:'hudson.plugin.git.GitSCM',plugin:'git') {
-                'userRemoteConfigs' {
-                    'hudson.plugin.git.UserRemoteConfig' {
-                        'url'('https://github.com/ZS-Omkar/users.git')
-                    }
-                }
-                'branches' {
-                    'hudson.plugin.git.BranchSpec' {
-                        'name'('*/main')
-                    }
-                }
-            }
-            'scriptPath'('Jenkinsfile')
-            'lightweight'(true)
-        }
-    }
-}
-
-pipelineJob("CI-Pipelines/login") {
-    configure { flowdefinition ->
-        flowdefinition << delegate. 'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
-            'scm'(class:'hudson.plugin.git.GitSCM',plugin:'git') {
-                'userRemoteConfigs' {
-                    'hudson.plugin.git.UserRemoteConfig' {
-                        'url'('https://github.com/ZS-Omkar/login.git')
-                    }
-                }
-                'branches' {
-                    'hudson.plugin.git.BranchSpec' {
-                        'name'('*/main')
-                    }
-                }
-            }
-            'scriptPath'('Jenkinsfile')
-            'lightweight'(true)
-        }
-    }
-}
-
-pipelineJob("CI-Pipelines/todo") {
-    configure { flowdefinition ->
-        flowdefinition << delegate. 'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
-            'scm'(class:'hudson.plugin.git.GitSCM',plugin:'git') {
-                'userRemoteConfigs' {
-                    'hudson.plugin.git.UserRemoteConfig' {
-                        'url'('https://github.com/ZS-Omkar/todo.git')
-                    }
-                }
-                'branches' {
-                    'hudson.plugin.git.BranchSpec' {
+                    'hudson.plugins.git.BranchSpec' {
                         'name'('*/main')
                     }
                 }
