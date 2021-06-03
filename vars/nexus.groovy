7b236388ed41d2a7e03f55d48b1681881952b311
Omkar <omkar.betha@gmail.com>
2021-06-03 16:45:36 +0530

    ss

--- vars/nexus.groovy
+++ vars/nexus.groovy
@@ -1,4 +1,4 @@
 def nexus() {
-    command = "curl -f -v -u admin:Omkar@123 --upload-file ${COMPONENT}.zip http://172.31.4.7:8081/repository/${COMPONENT}/${COMPONENT}.zip"
+    command = "curl -f -v -u admin:Omkar@123 --upload-file ${COMPONENT}.zip http://${args.NEXUS_IP}:8081/repository/${COMPONENT}/${COMPONENT}.zip"
     def execute_state=sh(returnStdout: true, script: command)
 }
