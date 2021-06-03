dffb679845234a6becd11763c2762ad5e03d761d
Omkar <omkar.betha@gmail.com>
2021-06-03 16:15:23 +0530

    ss

--- vars/todo.groovy
+++ vars/todo.groovy
@@ -1,9 +1,21 @@
-def call() {
+def call(Map params = [:]) {
+
+    def args = [
+            NESUS_IP        :'172.31.4.7',
+    ]
+    args << params
 
     pipeline {
         agent {
-            label 'JAVA'
+            label "${args.SLAVE_LABEL}"
         }
+
+        environment {
+            COMPONENT       = "${args.COMPONENT}"
+            NEXUS_IP        = "${args.NEXUS_IP}"
+            PROJECT_NAME    = "${args.PROJECT_NAME}"
+            SLAVE_LABEL     = "${args.SLAVE_LABEL}"
+            APP_TYPE        = "${args.APP_TYPE}"
 
         stages {
 
@@ -24,15 +36,15 @@ def call() {
             stage('Prepare Artifacts') {
                 steps {
                     sh '''
-                        zip -r frontend.zip *
+                        zip -r ${COMPONENT}.zip *
                     '''
                 }
             }
             stage('Upload Artifacts') {
                 steps {
-                    script {
-                        nexus
-                    }
+                    sh '''
+                    curl -v -u admin:Omkar@123 --upload-file /home/ubuntu/workspace/${COMPONENT}.zip http://${args.NEXUS_IP}:8081/repository/${COMPONENT}/${COMPONENT}.zip
+                    '''
                 }
             }
         }
