3faa272c5bea4f490bf6520da127b6e764ceb15d
Omkar <omkar.betha@gmail.com>
2021-06-03 19:52:18 +0530

    ss

--- vars/todo.groovy
+++ vars/todo.groovy
@@ -11,49 +11,37 @@ def call(Map params = [:]) {
         }
 
         environment {
-            COMPONENT = "${args.COMPONENT}"
-            NEXUS_IP = "${args.NEXUS_IP}"
-            PROJECT_NAME = "${args.PROJECT_NAME}"
-            SLAVE_LABEL = "${args.SLAVE_LABEL}"
-            APP_TYPE = "${args.APP_TYPE}"
+            COMPONENT        = "${args.COMPONENT}"
+            NEXUS_IP         = "${args.NEXUS_IP}"
+            PROJECT_NAME     = "${args.PROJECT_NAME}"
+            SLAVE_LABEL      = "${args.SLAVE_LABEL}"
+            APP_TYPE         = "${args.APP_TYPE}"
 
             stages {
-                stage('Download Dependencies') {
-                    when {
-                        environment name: 'COMPONENT', value: 'frontend'
-                    }
-
+                stage('Build Code & Install Dependencies') {
                     steps {
-                        sh '''
-          npm install
-        '''
-                    }
-                }
-                stage('Making build') {
-                    steps {
-                        sh '''
-                      npm run build
-                    '''
+                        script {
+                            build = new nexus()
+                            build.code_build("${APP_TYPE}", "${COMPONENT}")
+                        }
                     }
                 }
                 stage('Prepare Artifacts') {
                     steps {
-                        sh '''
-                        zip -r ${COMPONENT}.zip *
-                    '''
+                        script {
+                            prepare = new nexus()
+                            prepare.make_artifacts("${APP_TYPE}", "${COMPONENT}")
+                        }
                     }
                 }
                 stage('Upload Artifacts') {
                     steps {
-                        sh '''
-                    curl -v -u admin:Omkar@123 --upload-file /home/ubuntu/workspace/${COMPONENT}.zip http://${args.NEXUS_IP}:8081/repository/${COMPONENT}/${COMPONENT}.zip
-                    '''
+                        script {
+                            prepare = new nexus()
+                            prepare.nexus(COMPONENT)
+                        }
                     }
                 }
             }
-
         }
-
     }
-
-}
