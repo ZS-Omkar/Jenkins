50483dd8accf5cba8a31e2fb8b7dbe135ae39832
Omkar <omkar.betha@gmail.com>
2021-06-03 15:30:20 +0530

    ss

--- vars/nexus.groovy
+++ vars/nexus.groovy
@@ -2,4 +2,3 @@ def nexus() {
     command = "curl -v -u admin:Omkar@123 --upload-file /home/ubuntu/workspace/frontend.zip http://172.31.4.7:8081/repository/frontend/frontend1.zip"
     def execute_state=sh(returnStdout: true, script: command)
 }
-
---
+++ vars/todo.groovy
@@ -0,0 +1,41 @@
+def call() {
+
+    pipeline {
+        agent {
+            label 'JAVA'
+        }
+
+        stages {
+
+            stage('Download Dependencies') {
+                steps {
+                    sh '''
+          npm install
+        '''
+                }
+            }
+            stage('Making build') {
+                steps {
+                    sh'''
+          npm run build
+        '''
+                }
+            }
+            stage('Prepare Artifacts') {
+                steps {
+                    sh '''
+         zip -r frontend.zip *
+        '''
+                }
+            }
+            stage('Upload Artifacts') {
+                steps {
+                    script {
+                        nexus
+                    }
+                }
+            }
+        }
+    }
+
+}
