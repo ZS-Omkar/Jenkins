16d60d82ae00e3d9246b0efc81aa76e499f72bf8
Omkar <omkar.betha@gmail.com>
2021-06-08 12:44:36 +0530

    ss

--- vars/nexus.groovy
+++ vars/nexus.groovy
@@ -28,7 +28,7 @@ def code_build(APP_TYPE, COMPONENT){
         def execute_com=sh(returnStdout: true, script: command)
         print execute_com
     } else if(APP_TYPE == "GO"){
-        command = "export GOPATH=~/go
+        command = "export GOPATH=go
                   depmod && apt install go-dep
                   dep ensure && go get && go build"
         def execute_com=sh(returnStdout: true, script: command)
