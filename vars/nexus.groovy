fbe0b988839cf9ab3f6c83286f62e321af838d9f
Omkar <omkar.betha@gmail.com>
2021-06-25 16:28:10 +0530

    ss

--- vars/nexus.groovy
+++ vars/nexus.groovy
@@ -1,22 +1,30 @@
 def nexus(COMPONENT) {
-    command = "curl -f -v -u admin:Omkar@123 --upload-file ${COMPONENT}.zip http://${NEXUS_IP}:8081/repository/${COMPONENT}/${COMPONENT}.zip"
+    get_branch = "env | grep GIT_BRANCH | awk -F / '{print \$NF}' | xargs echo -n"
+    def get_branch_exec=sh(returnStdout: true, script: get_branch)
+    def FILENAME=COMPONENT+'-'+get_branch_exec+'.zip'
+
+    command = "curl -f -v -u admin:Omkar@123 --upload-file ${FILENAME} http://${NEXUS_IP}:8081/repository/${COMPONENT}/${FILENAME}"
     def execute_state=sh(returnStdout: true, script: command)
 }
+
 def make_artifacts(APP_TYPE, COMPONENT){
+    get_branch = "env | grep GIT_BRANCH | awk -F / '{print \$NF}' | xargs echo -n"
+    def get_branch_exec=sh(returnStdout: true, script: get_branch)
+    def FILENAME=COMPONENT+'-'+get_branch_exec+'.zip'
     if(APP_TYPE == "NGINX"){
-        command = "echo ${COMPONENT} && zip -r ${COMPONENT}.zip *"
+        command = "echo ${COMPONENT} && zip -r ${FILENAME} ."
         def execute_com=sh(returnStdout: true, script: command)
         print execute_com
     } else if(APP_TYPE == "GO"){
-        command = "zip -r ${COMPONENT}.zip *"
+        command = "zip -r ${FILENAME} ."
         def execute_com=sh(returnStdout: true, script: command)
         print execute_com
-    } else if(APP_TYPE == "JAVA"){
-        command = "cp target/*.jar ${COMPONENT}.jar && zip -r ${COMPONENT}.zip ${COMPONENT}.jar"
+    } else if(APP_TYPE == "MAVEN"){
+        command = "cp target/*.jar ${COMPONENT}.jar && zip -r ${FILENAME} ${COMPONENT}.jar"
         def execute_com=sh(returnStdout: true, script: command)
         print execute_com
     } else if(APP_TYPE == "NODEJS"){
-        command = "zip -r ${COMPONENT}.zip node_modules server.js"
+        command = "zip -r ${FILENAME} node_modules server.js"
         def execute_com=sh(returnStdout: true, script: command)
         print execute_com
     }
