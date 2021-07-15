51fc8acf86be9e8eec49fbe7d032468e028e8c22
Omkar <omkar.betha@gmail.com>
2021-07-15 10:45:43 +0530

    ss

--- vars/nexus.groovy
+++ vars/nexus.groovy
@@ -15,8 +15,8 @@ def make_artifacts(APP_TYPE, COMPONENT){
         command = "echo ${COMPONENT} && zip -r ${FILENAME} ."
         def execute_com=sh(returnStdout: true, script: command)
         print execute_com
-    } else if(APP_TYPE == "GO"){
-        command = "zip -r ${FILENAME} ."
+    } else if(APP_TYPE == "GOLANG") {
+        command = "zip -r ${FILENAME} * "
         def execute_com=sh(returnStdout: true, script: command)
         print execute_com
     } else if(APP_TYPE == "MAVEN"){
@@ -35,7 +35,7 @@ def code_build(APP_TYPE, COMPONENT){
         command = "npm install && npm run build"
         def execute_com=sh(returnStdout: true, script: command)
         print execute_com
-    } else if(APP_TYPE == "GO"){
+    } else if(APP_TYPE == "GOLANG") {
         command = "go build"
         def execute_com=sh(returnStdout: true, script: command)
         print execute_com
