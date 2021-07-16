cee348c2a48fa50c31237ebdc8534f5597351ba9
Omkar <omkar.betha@gmail.com>
2021-07-16 16:45:51 +0530

    ss

--- vars/nexus.groovy
+++ vars/nexus.groovy
@@ -6,7 +6,7 @@ def nexus(COMPONENT) {
     command = "ls"
 
 //   command = "curl -f -v -u admin:Omkar@123 --upload-file ${FILENAME} http://${NEXUS_IP}:8081/repository/${COMPONENT}/${FILENAME}"
-//    def execute_state=sh(returnStdout: true, script: command)
+   def execute_state=sh(returnStdout: true, script: command)
 }
 
 def make_artifacts(APP_TYPE, COMPONENT){
